dc97d4355c629400a2e57fd1cbce22a1d28e8a56
ayhanakkaya20242025 <ayhanakkaya20242025@gmail.com>
2024-10-17 07:36:00 +0000

    map yapısı anlatıldı

---
+++ lib/dersler/hafta5/hafta5_map.dart
@@ -0,0 +1,24 @@
+void main(List<String> args) {
+  var elementler = {
+    1: "Hidrojen",
+    3: "Lityum",
+  };
+  var baskentler = Map<String, String>();
+  //var soyGazlar = Map<int, String>();
+
+  elementler[4] = "berilyum";
+  baskentler["tr"] = "Ankara";
+  var soygazlar = {
+    2: "helyum",
+    10: "neon",
+    18: "argon",
+    32: "krypton",
+    36: "xenon",
+    44: "radon",
+    50: "plutonium",
+  };
+  elementler.addAll(soygazlar);
+  print(elementler);
+  print(baskentler);
+  print("Element Sayısı : ${elementler.length} dir");
+}
